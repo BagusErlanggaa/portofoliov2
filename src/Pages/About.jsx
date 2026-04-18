9347f41bd789a8e10a4d1bf7bc1c79531cece3af
BagusErlanggaa <muhammad.bagus56987@smk.belajar.id>
2026-04-18 17:31:15 +0700

    cv update

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -244,7 +244,7 @@ const AboutPage = () => {
       </div>
 
             <div className="flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-4 lg:px-0 w-full">
-              <a href="https://drive.google.com/file/d/1WgrNcjonM5uzRh44YCT6t8IzxXzJfkPh/view?usp=sharing" className="w-full lg:w-auto">
+              <a href="https://drive.google.com/file/d/1Jjl-igTsabVzkYXOl4lh0qPQBMkF2OJt/view?usp=sharing" className="w-full lg:w-auto">
               <button 
                 data-aos="fade-up"
                 data-aos-duration="800"
